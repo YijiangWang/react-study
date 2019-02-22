2dc90c7ac12f7d487d738ec25fae8c985788eae7
YIJIANGWANG\Administrator <wangyijiang@unipus.cn>
2019-02-22 15:52:07 +0800

    组件解耦-01

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import App from './app';
-import {reducer} from './index.redux';
+import {reducer, join, leave} from './index.redux';
 
 const store = createStore(reducer);
 
 const render = () => {
-  ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+  ReactDOM.render(<App store={store} join={join} leave={leave}/>, document.getElementById('root'));
 };
 render();
 store.subscribe(render);
